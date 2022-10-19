0441384baac1036ded66f6bb35560b6700c936f3
tahsincanpolat <tahsincanpolat@gmail.com>
2022-10-19 19:38:29 +0300

    users map endpoints

--- contact/Users/Program.cs
+++ contact/Users/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Users.DataAccess;
+using Users.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,28 +30,90 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/users", async ( UserDBContext db) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    if (db.Users.ToList() != null)
+    {
+        var users = await db.Users.ToListAsync();
+        return Results.Ok(users);
+    }
+    else
+    {
+        return Results.BadRequest("Error! Users not found.");
+    }
 
-app.Run();
+});
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+app.MapGet("/userById/{id}", async (Guid? id, UserDBContext db) =>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    if (id.HasValue)
+    {
+        var user = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
+        return Results.Ok(user);
+    }
+    else
+    {
+        return Results.BadRequest("Error! Please enter a valid id.");
+    }
+
+});
+
+app.MapPost("/userAdd", async (User user,UserDBContext db) =>
+{
+    await db.Users.AddAsync(user);
+    if (db.SaveChanges()>0)
+    {
+        return Results.Ok(user);
+    }
+    else
+    {
+        return Results.BadRequest("Error! Could not add user.");
+    }
+
+});
+
+app.MapDelete("/userDelete/{id}", async (Guid? id, UserDBContext db) =>
+{
+    User? getUser = await db.Users.FirstOrDefaultAsync(u => u.Id == id);
+    if (getUser == null)
+    {
+        return Results.BadRequest("Error! User not found.");
+    }
+    else
+    {
+        db.Users.Remove(getUser);
+        db.SaveChanges();
+        return Results.Ok("User Deleted.");
+    }
+
+});
+
+
+
+
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//       new WeatherForecast
+//       (
+//           DateTime.Now.AddDays(index),
+//           Random.Shared.Next(-20, 55),
+//           summaries[Random.Shared.Next(summaries.Length)]
+//       ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast");
+
+//app.Run();
+
+//internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
